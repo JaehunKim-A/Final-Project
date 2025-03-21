8468ece2f55ae4facedd79af9fcd84dd39eb0639
Yudir-98 <Wjsdbqja156@gmail.com>
2025-03-21 10:43:20 +0900

    Base Template first upload

---
+++ src/main/java/com/team1/team1project/controller/testController.java
@@ -0,0 +1,12 @@
+package com.team1.team1project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class testController {
+    @GetMapping("/test")
+    public String testPage() {
+        return "dist/index";
+    }
+}
