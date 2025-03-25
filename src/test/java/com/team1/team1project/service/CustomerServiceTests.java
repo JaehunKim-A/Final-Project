9beb21825c44fc23d3c237021b6997a8670151da
jiyeon <ringonhy213@gmail.com>
2025-03-25 10:33:27 +0900

    0325

---
+++ src/test/java/com/team1/team1project/service/CustomerServiceTests.java
@@ -0,0 +1,22 @@
+package com.team1.team1project.service;
+
+import com.team1.team1project.customer.domain.Customer;
+import com.team1.team1project.customer.service.CustomerService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CustomerServiceTests {
+	@Autowired
+	private CustomerService customerService;
+
+	@Test
+	public void testModify() {
+		Customer customer = new Customer(7, "ModifiedName", "Modified123-456-7890", "ModifiedAddress");
+
+		Customer result = customerService.updateCustomer(7, customer);
+	}
+
+
+}
---
+++ src/test/java/com/team1/team1project/service/RawMaterialSupplierServiceTests.java
@@ -0,0 +1,22 @@
+package com.team1.team1project.service;
+
+import com.team1.team1project.raw_material_suppliers.domain.RawMaterialSupplier;
+import com.team1.team1project.raw_material_suppliers.service.RawMaterialSupplierService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RawMaterialSupplierServiceTests {
+
+	@Autowired
+	private RawMaterialSupplierService rawMaterialSupplierService;
+
+	@Test
+	public void testMofify() {
+		RawMaterialSupplier rawMaterialSupplier = new RawMaterialSupplier(3, "ModifiedName", "MofifiedContact", "ModifiedAddress", "ModifiedEmail", "Modified123-456-7890");
+
+		RawMaterialSupplier result = rawMaterialSupplierService.updateRawMaterialSupplier(3, rawMaterialSupplier);
+
+	}
+}
